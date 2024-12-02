73e75329b5843bc10969d00f1825b0951772fefa
evgenii <vovk.evgenii.199811@gmail.com>
2024-12-02 12:00:55 +0300

    refactoring code

--- db/database.py
+++ db/database.py
@@ -4,26 +4,12 @@ import os
 
 _ = load_dotenv()
 
-connection = ps.connect(
-    host=os.getenv("HOST"),
-    user=os.getenv("USER"),
-    password=os.getenv("PASSWORD"),
-    database=os.getenv("DATABASE"),
-)
 
-cursor = connection.cursor()
-
-pr = """
-CREATE TABLE words (
-    word_id SERIAL PRIMARY KEY,
-    word_russian VARCHAR(255) NOT NULL,
-    word_translate VARCHAR(255) NOT NULL
-    );
-"""
-
-cursor.execute(pr)
-
-connection.commit()
-
-cursor.close()
-connection.close()
+def get_connection():
+    connection = ps.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+    )
+    return connection
---
+++ db/models.py
@@ -0,0 +1,54 @@
+from database import get_connection
+
+
+def init_db() -> None:
+    """
+    Создает подключение к базе данных
+    """
+
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        cursor.execute(
+            """
+                       CREATE TABLE IF NOT EXISTS words (
+                           word_id SERIAL PRIMARY KEY,
+                           word_russian VARCHAR(255) NOT NULL,
+                           word_english VARCHAR(255) NOT NULL
+                           );
+                           """
+        )
+        conn.commit()
+    conn.close
+
+
+def add_words(word_russian: str, word_english: str) -> None:
+    """
+    Добавляет новые слова
+    """
+
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        cursor.execute(
+            """INSERT INTO words (word_russian, word_english) VALUES (%s, %s)""",
+            (word_russian, word_english),
+        )
+        conn.commit()
+    conn.close
+
+
+def delete_words(word_russian: str) -> None:
+    """
+    Удаляет слово
+    """
+
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        cursor.execute(
+            """DELETE FROM words WHERE word_russian=%s;""",
+            (word_russian,),
+        )
+        conn.commit()
+    conn.close
